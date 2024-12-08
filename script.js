1eb42c7bd4b636fed7fcd8890f185a37f7b54c13
Alex_Kozlov <kau8800@gmail.com>
2024-12-08 20:29:14 +0200

    JS data types with logs

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Example of a string type
+const stringType = "Hello, world!";
+console.log("stringType:", typeof stringType);
+
+// Example of a number type
+const numberType = 42;
+console.log("numberType:", typeof numberType);
+
+// Example of a boolean type
+const booleanType = true;
+console.log("booleanType:", typeof booleanType);
+
+// Example of an undefined type
+const undefinedType = undefined;
+console.log("undefinedType:", typeof undefinedType);
+
+// Example of a null type
+const nullType = null;
+console.log("nullType:", typeof nullType);
+
+// Example of a symbol type
+const symbolType = Symbol("id");
+console.log("symbolType:", typeof symbolType);
+
+// Example of a bigint type
+const bigIntType = BigInt(123n);
+console.log("bigIntType:", typeof bigIntType);
+
+// Example of an object type
+const objectType = { name: "Object" };
+console.log("objectType:", typeof objectType);
